0d70e910758d7b0d88f51773b6258651723cdbfe
DoctorGurke <docgurk@gmail.com>
2021-08-29 00:54:03 +0200

    bool Entity.Alive extension

--- code/extensions/EntityExtensions.cs
+++ code/extensions/EntityExtensions.cs
@@ -5,5 +5,9 @@ namespace Equivalent.Util {
 		public static void Kill(this Entity ent) {
 			ent.TakeDamage(DamageInfo.Generic(float.MaxValue));
 		}
+
+		public static bool Alive(this Entity ent) {
+			return ent.LifeState == LifeState.Alive;
+		}
 	}
 }
