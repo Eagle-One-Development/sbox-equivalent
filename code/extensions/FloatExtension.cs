f71bd6fbcbc189870dd793f333cb898df44df409
DoctorGurke <docgurk@gmail.com>
2021-09-22 19:17:26 +0200

    Delete FloatExtension.cs

--- code/extensions/FloatExtension.cs
+++
@@ -1,7 +0,0 @@
-namespace Equivalent.Util {
-	public static class FloatExtensions {
-		public static float Remap(this float input, float inputMin, float inputMax, float min, float max) {
-			return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
-		}
-	}
-}
