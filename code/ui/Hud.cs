5803fc792aa594d6c2bc6d003c9d4d95d09feae6
DoctorGurke <docgurk@gmail.com>
2022-02-05 19:57:52 +0100

    template

--- code/ui/Hud.cs
+++ code/ui/Hud.cs
@@ -4,9 +4,7 @@ namespace Equivalent.UI {
 	public partial class EquivalentHud : Sandbox.HudEntity<RootPanel> {
 		public EquivalentHud() {
 			if(!IsClient) return;
-			RootPanel.AddChild<ChatBox>();
-			RootPanel.AddChild<NameTags>();
-			RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
+			RootPanel.SetTemplate("/ui/Hud.html");
 		}
 	}
 }
